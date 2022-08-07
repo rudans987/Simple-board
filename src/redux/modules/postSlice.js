0b9f919edb6eb25a6677393471084d92457bca8c
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-08 00:28:35 +0900

    삭제 기능 실시간 반영 안되는 오류 해결

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -105,7 +105,7 @@ const postSlice = createSlice({
         state.loading = true;
       })
       .addCase(__deletePost.fulfilled, (state, action) => {
-        return state.list.filter((post) => post.id !== action.payload);
+        state.list = state.list.filter((post) => post.id !== action.payload);
       })
       .addCase(__deletePost.rejected, (state, action) => {
         state.loading = false;
@@ -116,7 +116,7 @@ const postSlice = createSlice({
         state.loading = true;
       })
       .addCase(__updatePost.fulfilled, (state, action) => {
-        return state.list
+        state.list = state.list
           .map((post) => {
             if (post.id === action.payload.id) {
               return {
