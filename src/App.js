03494094bfc38cfef6146c8550771fb399fed7e4
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-07 22:21:24 +0900

    add App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import styled from "styled-components";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/add" element={<Detail />}></Route>
+      <Route path="/detail/:id" element={<Detail />}></Route>
+      <Route path="/update/:id" element={<Detail />}></Route>
+    </Routes>
   );
 }
 
