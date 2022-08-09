e1c73ea88d9c83be7feb2adc1cb386b260fa154e
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-09 13:48:32 +0900

    add infinite scrolling function

--- src/components/main/List.jsx
+++ src/components/main/List.jsx
@@ -1,12 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { __getPostList, __deletePost } from "../../redux/modules/postSlice";
 import ListItem from "./ListItem";
 import styled from "styled-components";
+import axios from "axios";
 
 function List() {
-  const dispatch = useDispatch();
   const postlist = useSelector((state) => state.postSlice.list);
+  const dispatch = useDispatch();
+
+  const [posts, setPost] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false); // use this if you want the box to say "loading...". Forgot this lol.
+  const [prevY, setPrevY] = useState(0);
+  let postsRef = useRef({});
+
+  let loadingRef = useRef(null);
+  let prevYRef = useRef({});
+  let pageRef = useRef({});
+  postsRef.current = posts;
+  pageRef.current = page;
+
+  prevYRef.current = prevY;
+
+  console.log("loadingRef: ", loadingRef);
+
+  useEffect(() => {
+    getPosts();
+    setPage(pageRef.current + 1);
+
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(handleObserver, options);
+    observer.observe(loadingRef.current);
+  }, []);
+
+  const handleObserver = (entities, observer) => {
+    const y = entities[0].boundingClientRect.y;
+
+    if (prevYRef.current > y) {
+      console.log("post를 가져온다");
+      getPosts();
+      setPage(pageRef.current + 1);
+    } else {
+    }
+    console.log("currenty: ", y, "prevY: ", prevY);
+    setPrevY(y);
+  };
+
+  const getPosts = async () => {
+    try {
+      let postsRetrieved = await axios.get(
+        `http://localhost:5001/list?_page=${pageRef.current}&_limit=5`
+      );
+      if (postsRetrieved) {
+        setPost([...postsRef.current, ...postsRetrieved.data]);
+      }
+    } catch (error) {
+      console.log("ERROR GETTING PHOTOS");
+    }
+  };
 
   useEffect(() => {
     dispatch(__getPostList());
@@ -15,10 +72,11 @@ function List() {
   const onDeleteHandler = (postId) => {
     dispatch(__deletePost(postId));
   };
+
   return (
     <>
       <StyledContainer>
-        {postlist.map((post) => {
+        {posts.map((post, index) => {
           return (
             <>
               <ListItem
@@ -29,13 +87,24 @@ function List() {
             </>
           );
         })}
+        <div
+          ref={loadingRef}
+          style={{
+            height: "100px",
+            margin: "25px",
+          }}
+        >
+          <span style={{ display: loading ? "block" : "none" }}>
+            Loading...
+          </span>
+        </div>
       </StyledContainer>
     </>
   );
 }
 
 const StyledContainer = styled.div`
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
