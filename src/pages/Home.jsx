046fa5cdd1911b485894cbc0de3049dbfe40e9b5
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-09 20:14:54 +0900

    header 경로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { __getTodoList } from "../redux/modules/postSlice";
 import Button from "../components/common/Button";
-import Header from "../components/main/Header";
+import Header from "../components/common/Header";
 import Layout from "../components/common/Layout";
 import List from "../components/main/List";
 
