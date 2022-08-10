d461bfa1653ce6d65eefd06dc58b68653963e88b
rudans987 <rudans987@naver.com>
2022-08-10 09:34:42 +0900

    Wrapper styled components

---
+++ src/components/common/Wrapper.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const WrapperForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid gray;
+  margin: 0 auto;
+  padding: 10px 10px;
+  margin-top: 1rem;
+  width: 700px;
+`;
+
+export const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+border: 2px solid gray;
+margin: 0 auto;
+padding: 10px 10px;
+margin-top: 1rem;
+width: 700px;
+.title {
+  display: block;
+}
+`;
