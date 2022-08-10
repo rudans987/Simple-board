446a0ce19a76499e1545e4da116cfeae3c023d33
rudans987 <rudans987@naver.com>
2022-08-10 09:33:28 +0900

    useInput 커스텀 훅 사용

--- src/components/comments/AddComment.js
+++ src/components/comments/AddComment.js
@@ -1,50 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import TextField from '@mui/material/TextField';
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+import useInput from '../hooks/useinput';
 import { __addComment } from "../../redux/modules/commentsSlice";
 import Button from '../common/Button';
-
+import {WrapperForm} from "../common/Wrapper";
 
 
 const AddCommentForm = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const { 
+  const {
     register,
     handleSubmit,
     formState: { isDirty, errors }
   } = useForm();
-  const [comment, setComment] = useState({
-    username: "",
-    content: "",
-  });
 
   const onAddCommentButtonHandler = (data) => {
-
     dispatch(__addComment({
       todoId: id,
       username: data.username,
       content: data.content
     }));
-    setComment({
-      username: "",
-      content: "",
-    });
+    setUsername("");
+    setContent("");
   };
-
-  const onChangeInputHandler = (event) => {
-    const { name, value } = event.target;
-    setComment({
-      ...comment,
-      [name]: value,
-    });
-  };
-
+  const [username, setUsername, onChangeUsernameHandler] = useInput();
+  const [content, setContent, onChangeContentHandler] = useInput();
+ 
   return (
+    
     <WrapperForm onSubmit={handleSubmit(onAddCommentButtonHandler)}>
       <TextField
         required
@@ -60,10 +49,10 @@ const AddCommentForm = () => {
             message: "5자 이내여야합니다.",
           },
         })}
-        value={comment.username}
+        value={username}
         type='text'
         name='username'
-        onChange={onChangeInputHandler}
+        onChange={onChangeUsernameHandler}
       />
       {errors.username && <small role="alert">{errors.username.message}</small>}
 
@@ -81,32 +70,29 @@ const AddCommentForm = () => {
             message: "100자 이내로  작성해주세요.",
           },
         })}
-        value={comment.content}
+        value={content}
         name='content'
         type='text'
-        onChange={onChangeInputHandler}
+        onChange={onChangeContentHandler}
       />
       {errors.content && <small role="alert">{errors.content.message}</small>}
       <Button type='submit'>
         추가하기
       </Button>
     </WrapperForm>
+  
   );
 };
 
 export default AddCommentForm;
 
-const WrapperForm = styled.form`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 2px solid gray;
-  margin: 0 auto;
-  padding: 10px 10px;
-  margin-top: 1rem;
-  width: 700px;
-  
-  .title {
-    display: block;
-  }
-`;
+// const WrapperForm = styled.form`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   border: 2px solid gray;
+//   margin: 0 auto;
+//   padding: 10px 10px;
+//   margin-top: 1rem;
+//   width: 700px;
+// `;
